fcc0cf43839010c93d565caf4d33cbef2986ef0b
苏杰豪 <Megasu@qq.com>
2023-08-08 15:44:34 +0800

    fix: timeout defaults

--- src/defaults/index.ts
+++ src/defaults/index.ts
@@ -2,6 +2,6 @@ import type { UnConfig } from '../types';
 
 export const defaults: Partial<UnConfig> = {
   adapter: 'request',
-  timeout: 0,
+  timeout: 60000,
   validateStatus: (status) => status >= 200 && status < 300,
 };
